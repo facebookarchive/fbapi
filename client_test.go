e38e42774e30ac39aaeea8572a595b57e88d2fd7
Naitik Shah <n@daaku.org>
2013-08-02 23:48:39 -0700

    test nil urls

--- client_test.go
+++ client_test.go
@@ -100,3 +100,52 @@ func TestInvalidGet(t *testing.T) {
 		t.Fatalf("was expecting status 404 but got %d", res.StatusCode)
 	}
 }
+
+func TestNilURLWithDefaultBaseURL(t *testing.T) {
+	t.Parallel()
+	res, err := defaultFbClient.Do(&http.Request{Method: "GET"}, nil)
+	if err == nil {
+		t.Fatal("was expecting error")
+	}
+
+	const expected = `GET https://graph.facebook.com/ got 400 Bad Request ` +
+		`failed with code 100 type GraphMethodException message Unsupported get ` +
+		`request`
+
+	if err.Error() != expected {
+		t.Fatalf(`expected "%s" got "%s"`, expected, err)
+	}
+
+	if res.StatusCode != 400 {
+		t.Fatalf("was expecting status 400 but got %d", res.StatusCode)
+	}
+}
+
+func TestNilURLWithBaseURL(t *testing.T) {
+	t.Parallel()
+	client := &fbapi.Client{
+		BaseURL: &url.URL{
+			Scheme: "https",
+			Host:   "graph.facebook.com",
+			Path:   "/20aa2519-4745-4522-92a9-4522b8edf6e9",
+		},
+	}
+	res, err := client.Do(&http.Request{Method: "GET"}, nil)
+	if err == nil {
+		t.Fatal("was expecting error")
+	}
+
+	const expected = `GET ` +
+		`https://graph.facebook.com/20aa2519-4745-4522-92a9-4522b8edf6e9 got ` +
+		`404 Not Found failed with code 803 type OAuthException message (#803) ` +
+		`Some of the aliases you requested do not exist: ` +
+		`20aa2519-4745-4522-92a9-4522b8edf6e9`
+
+	if err.Error() != expected {
+		t.Fatalf(`expected "%s" got "%s"`, expected, err)
+	}
+
+	if res.StatusCode != 404 {
+		t.Fatalf("was expecting status 404 but got %d", res.StatusCode)
+	}
+}
