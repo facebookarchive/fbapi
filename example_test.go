9a4e661ff1afd1b962571da12400154a9edf5ef6
Naitik Shah <n@daaku.org>
2013-08-02 23:25:42 -0700

    package example

---
+++ example_test.go
@@ -0,0 +1,27 @@
+package fbapi_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/daaku/go.fbapi"
+)
+
+func Example() {
+	client := &fbapi.Client{}
+	var user struct {
+		ID   uint64 `json:"id"`
+		Name string `json:"name"`
+	}
+	_, err := client.Do(
+		&http.Request{Method: "GET", URL: &url.URL{Path: "naitik"}},
+		&user,
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("%+v", user)
+}
